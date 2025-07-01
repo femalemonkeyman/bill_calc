7d02bded21d06e35a356e4875932b383c6941ae4
femalemonkeyman <joeymacdonald55@gmail.com>
2025-07-01 11:40:24 -0400

    Added time picker for easier calculations

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"8ed9edbd7aaadf3c101281d43579d617b859e2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2012769444"
+    serviceWorkerVersion: "417637232"
   }
 });
