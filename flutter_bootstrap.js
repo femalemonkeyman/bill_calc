d178279f330bf88fb1cffd0123f717173353b1b0
femalemonkeyman <joeymacdonald55@gmail.com>
2026-06-19 13:00:00 -0400

    Corrected values for QST

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1989833405" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1821407712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
